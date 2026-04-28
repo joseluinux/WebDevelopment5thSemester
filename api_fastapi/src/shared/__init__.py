0838a28508408525b0a27e18a071ae9c23ab912b
Victor-ro-dev10 <victor.ro.dev10@gmail.com>
2026-04-28 17:10:43 -0300

    feat: add interfaces, schemas , logs and config

---
+++ api_fastapi/src/shared/__init__.py
@@ -0,0 +1,8 @@
+# ========================================================================
+# FILE: shared/__init__.py
+# PURPOSE: Marca pasta 'shared' como pacote Python
+# ========================================================================
+
+# Permite imports como:
+# from shared.logger import logger
+# from shared.exceptions import LumemeiException
